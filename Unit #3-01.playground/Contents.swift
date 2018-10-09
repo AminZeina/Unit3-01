f934d414ae791893020d8d8cec07a6247bf67c21
Amin Zeina <number10.10@hotmail.com>
2018-10-09 16:43:25 -0400

    Initial commit

---
+++ Unit #3-01.playground/Contents.swift
@@ -0,0 +1,78 @@
+// Created on: Oct 2018
+// Created by: Amin Zeina
+// Created for: ICS3U
+// This program lets user guess the number 
+
+// this will be commented out when code moved to Xcode
+import PlaygroundSupport
+
+
+import UIKit
+
+class ViewController : UIViewController {
+    // this is the main view controller, that will show the UIKit elements
+    
+    // properties
+    let NUMBER : Int = 11
+    
+    let instructionLabel = UILabel()
+    let numberGuessedTextField = UITextField()
+    let checkButton = UIButton()
+    let answerLabel = UILabel()
+    
+    override func viewDidLoad() {
+        // UI
+        super.viewDidLoad()
+        
+        let view = UIView()
+        view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        self.view = view
+        
+        instructionLabel.text = "Guess the number"
+        view.addSubview(instructionLabel)
+        instructionLabel.translatesAutoresizingMaskIntoConstraints = false
+        instructionLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
+        instructionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        
+        numberGuessedTextField.borderStyle = UITextBorderStyle.roundedRect
+        numberGuessedTextField.placeholder = "Enter a number"
+        view.addSubview(numberGuessedTextField)
+        numberGuessedTextField.translatesAutoresizingMaskIntoConstraints = false
+        numberGuessedTextField.topAnchor.constraint(equalTo: instructionLabel.bottomAnchor, constant: 20).isActive = true
+        numberGuessedTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        
+        checkButton.setTitle("Calculate", for: .normal)
+        checkButton.setTitleColor(.blue, for: .normal)
+        checkButton.titleLabel?.textAlignment = .center
+        checkButton.addTarget(self, action: #selector(checkNumberGuessed), for: .touchUpInside)
+        view.addSubview(checkButton)
+        checkButton.translatesAutoresizingMaskIntoConstraints = false
+        checkButton.topAnchor.constraint(equalTo: numberGuessedTextField.bottomAnchor, constant: 20).isActive = true
+        checkButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        
+        answerLabel.text = nil
+        view.addSubview(answerLabel)
+        answerLabel.translatesAutoresizingMaskIntoConstraints = false
+        answerLabel.topAnchor.constraint(equalTo: checkButton.bottomAnchor, constant: 20).isActive = true
+        answerLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+    }
+    
+    @objc func checkNumberGuessed() {
+        
+        let numberEntered : Int = Int(numberGuessedTextField.text!)!
+        
+        if numberEntered == NUMBER {
+            answerLabel.text = "Correct!"
+        } else {
+            answerLabel.text = "Try again."
+        }
+        
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
+
+// this will be commented out when code moved to Xcode
+PlaygroundPage.current.liveView = ViewController()
